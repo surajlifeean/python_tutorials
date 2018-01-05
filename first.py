050f83f71ada5518fb3f59e70b0c373f29a30e6f
surajlifeean <surajjeswara@yahoo.com>
2018-01-06 00:02:14 +0530

    A simple guesing game

---
+++ first.py
@@ -0,0 +1,30 @@
+import random
+
+
+number = random.randint(1,10)
+tries=1
+uname=input("Hello, What is ur user name?")
+
+
+
+print("Hello",uname+".",)
+
+
+
+question=input("Woould you like to play a game(Y/N)?")
+
+if question == 'N' or question == 'n':
+    print("Ok.. Bye")
+
+if question == 'Y' or question == 'y':
+    guess=int(input("Can You guess the number I am thinking?"))
+   
+    while guess!=number:
+        tries+=1
+        if guess>number:
+             print("Guess Lower...")
+        if guess<number:
+             print("Guess Higher")      
+        guess=int(input("Thats wrong! Try Again?"))
+       
+    print("well done you guessed it right in",tries,"tries")
